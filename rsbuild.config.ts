24932c95e3ad2ed831f37a00f3a7c8f6376dbf02
Stephan Kulla <git.mail@kulla.me>
2025-04-26 18:00:42 +0200

    feat(config): Add title

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -2,6 +2,9 @@ import { defineConfig } from '@rsbuild/core'
 import { pluginReact } from '@rsbuild/plugin-react'
 
 export default defineConfig({
+  html: {
+    title: 'Prototype',
+  },
   output: {
     assetPrefix: '/rsbuild-react-bun-biome-starter-template/',
   },
