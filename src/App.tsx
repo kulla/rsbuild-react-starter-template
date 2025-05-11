0900baad930b817f3ead02c14f06bb6414387293
Stephan Kulla <git.mail@kulla.me>
2025-05-11 22:44:20 +0200

    feat: Add tailwindcss/typography

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import './App.css'
 
 const App = () => {
   return (
-    <main className="content p-10">
+    <main className="prose p-10">
       <h1>Rsbuild with React</h1>
       <p>Start building amazing things with Rsbuild.</p>
     </main>
