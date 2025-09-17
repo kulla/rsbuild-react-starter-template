4892e5f6d4d579ec14332f6e2d7427a29edf4eb7
Stephan Kulla <git.mail@kulla.me>
2025-09-17 15:25:42 +0200

    refactor(style): Remove @tailwindcss/typography

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import './App.css'
 
 export default function App() {
   return (
-    <main className="prose p-10">
+    <main className="p-10">
       <h1>Rsbuild with React</h1>
       <p>Start building amazing things with Rsbuild.</p>
     </main>
